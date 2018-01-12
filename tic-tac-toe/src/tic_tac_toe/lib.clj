0d649a3507eef7d5de4e4d5bf8ae3609ff658ab3
Federico Caprari <korsmakolnikov@gmail.com>
2018-01-12 22:40:06 +0100

    Create basic template prj

---
+++ tic-tac-toe/src/tic_tac_toe/lib.clj
@@ -0,0 +1,8 @@
+(ns tic-tac-toe.lib)
+;; matrice -> get-indici-righe >> get-indici-colonne >> get-indici-diagonali ->
+;; riduci risultato per ogni funzione (false|vincitore) -> riduci le
+;; riduzioni (stallo|vincitore)
+
+(defn check-line
+  [line]
+  "Implementami!")
